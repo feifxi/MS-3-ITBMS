a4884eec7791858c813aee62921aeac3f4a14c79
Mevised117 <piyachokeice@gmail.com>
2025-05-04 00:58:13 +0700

    117-update ui

--- itbms-frontend/src/components/CardProduct.vue
+++ itbms-frontend/src/components/CardProduct.vue
@@ -14,23 +14,23 @@ const formatNumber = (num)=>{
 <template>
     <RouterLink
     :to="`/sale-items/${item.id}`"
-    class="Itbms-row block bg-white rouded-lg shadow-md hover:shadow-2xl transition-shadow duration-200 overflow-hidden"
+    class="itbms-row block bg-white rouded-lg shadow-md hover:shadow-2xl transition-shadow duration-200 overflow-hidden"
     >
     <!-- <img src="item.image || 'htttps://via.placeholder.com/300x300' " 
     alt="Product Image "
     class="w-full h-64 object-cover"/> -->
 
     <div class="p-4 space-y-2">
-        <p class="text-sm text-gray-600 Items-brand ">{{ item.brandName || 'No Brand' }}</p>
-        <h3 class="text-lg font-bold text-gray-900 Itbms-model">{{ item.model }}</h3>
+        <p class="text-sm text-gray-600 items-brand ">{{ item.brandName || 'No Brand' }}</p>
+        <h3 class="text-lg font-bold text-gray-900 itbms-model">{{ item.model }}</h3>
         <p class="text-sm text-gray-700 ">
-            <span class="Itmbs-ramGb">{{ item.ramGb }}</span> /
-            <span class="Itmbs-storageGb">{{ item.storageGb }}</span>
-            <span class="Itmbs-storageGb-unit">GB</span>
+            <span class="itmbs-ramGb">{{ item.ramGb || '-' }}</span> /
+            <span class="itmbs-storageGb">{{ item.storageGb }}</span>
+            <span class="itmbs-storageGb-unit">GB</span>
         </p>
-        <p class="text-xl font-semibold text-blue-600 Itbms-price" >
+        <p class="text-xl font-semibold text-blue-600 itbms-price" >
             {{ formatNumber(item.price) }}
-            <span class="text-sm text-gray-600 Itbms-price-unit">Baht</span>
+            <span class="text-sm text-gray-600 itbms-price-unit">Baht</span>
         </p>
     </div>
 </RouterLink>

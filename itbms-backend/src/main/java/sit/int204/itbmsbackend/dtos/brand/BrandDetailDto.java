f58e926818825aac8f9d95a70f5e6a0487efbeb7
hello <feoy1234@gmail.com>
2025-05-25 00:41:46 +0700

    fixbug in brand be

--- itbms-backend/src/main/java/sit/int204/itbmsbackend/dtos/brand/BrandDetailDto.java
+++ itbms-backend/src/main/java/sit/int204/itbmsbackend/dtos/brand/BrandDetailDto.java
@@ -1,6 +1,8 @@
 package sit.int204.itbmsbackend.dtos.brand;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,9 +17,16 @@ import java.util.Set;
 @Setter
 public class BrandDetailDto {
     private Integer id;
+    @NotBlank(message = "Name is required")
+    @Size(min = 1, max = 30)
     private String name;
+
+    @Size(max = 40, message = "websiteUrl must less than 40 characters")
     private String websiteUrl;
+
     private Boolean isActive;
+
+    @Size(max = 80, message = "countryOfOrigin must less than 80 characters")
     private String countryOfOrigin;
     @JsonIgnore
     private Set<SaleItem> saleItems = new LinkedHashSet<>();

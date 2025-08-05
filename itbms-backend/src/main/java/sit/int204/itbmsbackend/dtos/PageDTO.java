3c04d443aae3b3c6f812ff17d154a3c71d11a048
feifxi <feoy1234@gmail.com>
2025-08-05 18:27:18 +0700

    remove unneccessaries

--- itbms-backend/src/main/java/sit/int204/itbmsbackend/dtos/PageDTO.java
+++ itbms-backend/src/main/java/sit/int204/itbmsbackend/dtos/PageDTO.java
@@ -1,13 +1,12 @@
 package sit.int204.itbmsbackend.dtos;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.Setter;
+
+import lombok.Data;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 public class PageDTO<T> {
     private List<T> content;
     private Boolean last;

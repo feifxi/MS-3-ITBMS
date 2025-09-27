47e29d91b5e6e96c7ffe735365845a52be8b6895
feifxi <feoy1234@gmail.com>
2025-09-27 17:04:50 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-docker compose up -d --build --remove-orphans \
-&& docker exec itbms-ollama ollama pull llama3 \
+docker compose build --no-cache --pull \
+&& docker compose up -d --remove-orphans
+
+# wait a few seconds for container to start
+sleep 5
+
+docker exec itbms-ollama ollama pull llama3 \
 && docker image prune -f
